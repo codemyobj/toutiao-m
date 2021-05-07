b7b595cefcdc9dfa86c9f49fdb160baf7f00c86e
codemyobj <459324704@qq.com>
2021-05-07 11:49:15 +0800

    调整初始项目结构

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+import './assets/css/index.less'
+
 Vue.config.productionTip = false
 
+//创建vue根实例
+//把app根组件渲染到#app节点 
 new Vue({
   router,
   store,
